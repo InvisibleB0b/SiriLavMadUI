2f0d6121879ae09881548d60e3afc0eb52654e43
Revilo321 <oliverwii@live.dk>
2020-11-24 11:34:44 +0100

    Initial show history

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,21 +1,49 @@
+import axios,{
+AxiosResponse,
+AxiosError
+} from "../../node_modules/axios/index"
+
+
+interface IRecipe{
+    "Id":number,
+    "Title":string,
+    "LastMade":Date
+
+
+
+}
+
+const baseUrl: string = "http://localhost:55980/";
+
+
+
+
 new Vue({
     // TypeScript compiler complains about Vue because the CDN link to Vue is in the html file.
     // Before the application runs this TypeScript file will be compiled into bundle.js
     // which is included at the bottom of the html file.
     el: "#app",
     data: {
-        name: "",
-        greeting: ""
+        history: []
+
     },
-    methods: {
-        sayHello() {
-            console.log("Say Hello " + this.name)
-            if (this.name == "") {
-                this.greeting = "Hello NoName"
-            }
-            else {
-                this.greeting = "Hello " + this.name
-            }
+
+    mounted: async function () {
+        try{this.history = await (await axios.get<IRecipe[]>(baseUrl + "recipe/history")).data}
+        catch (error:AxiosError){
+        console.log(error);
         }
+        
+        console.log(this.history)
+    },
+
+    methods: {
+        
+
+
+
+
+
+
     }
 })
